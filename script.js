1c55acb0c6d649778ed885d35218257975eaea2a
Sourav93-subh <redsouravsubham@gmail.com>
2024-12-29 17:54:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Example of adding smooth scroll to anchor links for a better user experience
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
